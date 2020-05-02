0caa23598f9373dc2a210324178f215763d4ef7b
Kurt Galvin <kurtgalvin@gmail.com>
2020-05-02 16:55:40 +0000

    Fix syntax error

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -3,8 +3,8 @@ VALUES (5, 'Trump Duckie', 'Its really really great!', 2150, 'https://www.amster
   (2, 'Pumpkin',	'Great for halloween!',	1750,	'https://cdn11.bigcommerce.com/s-nf2x4/images/stencil/1280x1280/products/180/9687/Pumpkin-Rubber-Duck-Adline-3__42269.1568315315.jpg?c=2&imbypass=on',	'Richmond, BC'),
   (2, 'Unicorn Duckie',	'Cute as can be, but watch out for the horn',	1850,	'https://partycity6.scene7.com/is/image/PartyCity/_pdp_sq_?$_1000x1000_$&$product=PartyCity/788220_01',	'Vancouver, BC'),
   (5, 'Donut Duckies',	'Share with your friends, lot of 4.',	4200,	'https://i.pinimg.com/originals/1c/7c/1c/1c7c1ccb5b39e71e8436bd509fb83fb2.jpg',	'New Westminister, BC'),
-  (5, 'Biker Duckies',	'So cool you wont believe it.  Lot of 4.',	4000	'https://s7.orientaltrading.com/is/image/OrientalTrading/VIEWER_IMAGE_400/vinyl-biker-rubber-duckies~16_883d', 'Squamish, BC'),
-  (2,	'Pretty in Pink',	'So cuuuuute!!',	1500	'https://cdn.shopify.com/s/files/1/0251/3719/products/40611_1024x1024.jpg?v=1578543707',	'Vancouver, BC'),
+  (5, 'Biker Duckies',	'So cool you wont believe it.  Lot of 4.',	4000,	'https://s7.orientaltrading.com/is/image/OrientalTrading/VIEWER_IMAGE_400/vinyl-biker-rubber-duckies~16_883d', 'Squamish, BC'),
+  (2,	'Pretty in Pink',	'So cuuuuute!!',	1500,	'https://cdn.shopify.com/s/files/1/0251/3719/products/40611_1024x1024.jpg?v=1578543707',	'Vancouver, BC'),
   (1,	'Fire-Duckie',	'For all your kitten saving needs.',	1600,	'https://www.qualitylogoproducts.com/toys/fireman-rubber-duckie-hq.jpg?size=carousel',	'Delta, BC'),
   (5,	'Cowboy Duckie',	'Yeee haw!',	1900,	'https://stoysnetcdn.com/schy/schyrdkc/schyrdkc_3.jpg',	'North Vancouver, BC'),
   (5,	'Giant Duckie',	'Over 10 ft tall!',	89900,	'https://sc01.alicdn.com/kf/Hf1c5b42ca58d4c24959f5622dfad0471f.jpg',	'Surrey, BC')
