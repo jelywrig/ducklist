e63089bb63ca121ff50288c0e94259475a67dd7f
Kurt Galvin <kurtgalvin@gmail.com>
2020-05-03 17:45:15 +0000

    Replace empty heart with filled in heart for favorite

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,13 +6,17 @@ const escape = function(string){
 }
 
 const build_listing = function(listing) {
+  console.log(listing);
   return `
     <div class="card mt-4" style="width: 20rem;">
       <img src="${escape(listing.thumbnail_image_url)}" class="card-img-top">
       <div class="card-body">
         <h5 class="card-title">${escape(listing.title)}</h5>  <span>$${escape((listing.price_in_cents / 100).toFixed(2))}</span>
         <p class="card-text">${escape(listing.description)}</p>
-        <a href="#" class="btn btn-primary">Contact</a> <span class="align-middle float-right"><i class="material-icons">favorite_border</i></span>
+        <a href="#" class="btn btn-primary">Contact</a>
+        <span class="align-middle float-right">
+          <i class="material-icons">${listing.favourite ? 'favorite' : 'favorite_border'}</i>
+        </span>
       </div>
     </div>
   `
