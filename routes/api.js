776acbcaca25e0700541f0d1436cd86fbe8f29f8
Jessie Wright <jessie.wright@gmail.com>
2020-05-03 20:20:52 +0000

    added post route for listings and updated form handler

--- routes/api.js
+++ routes/api.js
@@ -58,6 +58,20 @@ module.exports = (db) => {
       })
   })
 
+
+  router.post("/listings", (req, res) => {
+
+    const queryParams = [req.session.user_id, req.body.title, req.body.description, req.body.image_url, req.body.price * 100];
+
+    db.query("INSERT INTO items (owner_id, title, description, thumbnail_image_url, price_in_cents) VALUES ($1,$2,$3,$4,$5) RETURNING * ", queryParams)
+    .then(data => {
+      res.json(data.rows[0]);
+;    });
+
+  });
+
+
+
   // TODO: Refine query
 
 
