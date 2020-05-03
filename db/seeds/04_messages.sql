863f8e146049b85fb3ea596f9d5f895a564f5b47
Jessie Wright <jessie.wright@gmail.com>
2020-05-03 18:33:22 +0000

    added another line of message data

--- db/seeds/04_messages.sql
+++ db/seeds/04_messages.sql
@@ -5,12 +5,6 @@ VALUES (4, 5, 'What are the actual dimensions of the giant duckie?', 9),
 (1, 2, 'Yup it sure is!', 7),
 (2, 1, 'Great, where can we meet for pickup?', 7),
 (3, 1, 'Is the fireman still up for sale?', 7),
-(1, 3, 'It''s currently pending pickup', 7);
-
-
-
-
-
-
-
+(1, 3, 'It''s currently pending pickup', 7),
+(1, 5, 'Cool bikers!', 5);
 
