bc4e0b8df616cf329a0f717dbdfc957b7a4f01f1
Kurt Galvin <kurtgalvin@gmail.com>
2020-05-04 18:58:47 +0000

    Create empty modal on click

--- public/scripts/conversations_modal.js
+++ public/scripts/conversations_modal.js
@@ -1,5 +1,30 @@
+const buildModal = function() {
+  const $modal = $(`
+    <div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalScrollableTitle">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
 
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `);
+  $modal.modal('toggle');
+  return $modal
+}
 
 const openConversationsModal = function(event) {
-  console.log('hello')
+  event.preventDefault()
+  $('#conversations-modal-container').html(buildModal())
 }
