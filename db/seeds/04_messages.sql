d505aa7614713edea235e91c459d429bdf43d4bc
Jessie Wright <jessie.wright@gmail.com>
2020-05-04 04:14:32 +0000

    updated seed data with timestamps

--- db/seeds/04_messages.sql
+++ db/seeds/04_messages.sql
@@ -1,10 +1,12 @@
-INSERT INTO messages (from_user, to_user, content, re_item)
-VALUES (4, 5, 'What are the actual dimensions of the giant duckie?', 9),
-(5, 4, 'I am honestly not sure, it is too tall for me to measure properly. It is huge!', 9),
-(2, 1, 'Is the fireman duckie still available?' ,7),
-(1, 2, 'Yup it sure is!', 7),
-(2, 1, 'Great, where can we meet for pickup?', 7),
-(3, 1, 'Is the fireman still up for sale?', 7),
-(1, 3, 'It''s currently pending pickup', 7),
-(1, 5, 'Cool bikers!', 5);
+INSERT INTO messages (from_user, to_user, content, re_item, sent_at)
+VALUES (4, 5, 'What are the actual dimensions of the giant duckie?', 9, make_timestamp(2020, 05, 03, 12, 30, 00.00)),
+(5, 4, 'I am honestly not sure, it is too tall for me to measure properly. It is huge!', 9, make_timestamp(2020, 05, 03, 12, 30, 00.10)),
+(2, 1, 'Is the fireman duckie still available?' ,7, make_timestamp(2020, 05, 03, 12, 30, 00.20)),
+(1, 2, 'Yup it sure is!', 7, make_timestamp(2020, 05, 03, 12, 30, 00.30)),
+(2, 1, 'Great, where can we meet for pickup?', 7, make_timestamp(2020, 05, 03, 12, 30, 00.40)),
+(3, 1, 'Is the fireman still up for sale?', 7, make_timestamp(2020, 05, 03, 12, 30, 00.50)),
+(1, 3, 'It''s currently pending pickup', 7, make_timestamp(2020, 05, 03, 12, 30, 01.00)),
+(1, 5, 'Cool bikers!', 5, make_timestamp(2020, 05, 03, 12, 30, 01.10)),
+(5, 1,'Is the cowboy still for sale?',8,make_timestamp(2020, 05, 03, 12, 30, 01.20))
+
 
