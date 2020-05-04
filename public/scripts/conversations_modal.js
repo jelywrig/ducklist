c14f4b9ed280abde641cc79c4dabfcdd9364b25a
Kurt Galvin <kurtgalvin@gmail.com>
2020-05-04 18:59:59 +0000

    Refactor empty modal on click

--- public/scripts/conversations_modal.js
+++ public/scripts/conversations_modal.js
@@ -4,7 +4,7 @@ const buildModal = function() {
       <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalScrollableTitle">Modal title</h5>
+            <h5 class="modal-title" id="exampleModalScrollableTitle">Conversations</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
@@ -14,7 +14,6 @@ const buildModal = function() {
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
           </div>
         </div>
       </div>
