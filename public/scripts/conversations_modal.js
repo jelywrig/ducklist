b218b7fbad9af711797acbdd2e97728c3e5cf09b
Kurt Galvin <kurtgalvin@gmail.com>
2020-05-04 21:34:42 +0000

    Fill out conversation values

--- public/scripts/conversations_modal.js
+++ public/scripts/conversations_modal.js
@@ -1,14 +1,14 @@
 
-const buildConversation = function({ title, sent_at }) {
+const buildConversation = function({ title, sent_at, content, user_id, from_user, from_user_id }) {
   // console.log(new Date(sent_at))
   const $conversation = $(`
     <a href="#" class="list-group-item list-group-item-action">
       <div class="d-flex w-100 justify-content-between">
-        <h5 class="mb-1">${title}</h5>
+        <h5 class="mb-1">${escape(title)}</h5>
         <small>${displayDate(sent_at)}</small>
       </div>
-      <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-      <small>Donec id elit non mi porta.</small>
+      <p class="mb-1" style="font-weight: bold;">${escape(content)}</p>
+      <small>From: ${user_id === from_user_id ? 'Me' : from_user}</small>
     </a>
   `);
   return $conversation;
@@ -17,7 +17,7 @@ const buildConversation = function({ title, sent_at }) {
 const buildModalBody = function() {
   return new Promise((resolve, reject) => {
     $.get("/api/messages/summaries", data => {
-      // console.log(data.messages)
+      console.log(data.messages)
       resolve(data.messages.map(buildConversation))
     })
   })
