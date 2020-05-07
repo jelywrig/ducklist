efdbfdccae0ba51cfdb994f91f8c57dad5ade1df
Jessie Wright <jessie.wright@gmail.com>
2020-05-07 00:46:35 +0000

    initial login and register routes

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,7 +1,7 @@
 -- Users table seeds here (Example)
 INSERT INTO users (first_name, last_name, email, phone, password, is_admin)
-VALUES ('Kurt', 'Galvin', 'kurtgalvin@gmail.com', '+16047733327','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', TRUE),
-('Jessie', 'Wright', 'jessie.wright@gmail.com', '+16044013694','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', TRUE),
-('Sally', 'Fieldman', 'sally@example.com', '','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', FALSE),
-('Mike', 'Doorman', 'mike@example.com', '','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', FALSE),
-('Duck', 'Baron', 'baron@example.com', '+16044013694','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', TRUE)
+VALUES ('Kurt', 'Galvin', 'kurtgalvin@gmail.com', '+16047733327','$2b$10$EY5gfFrs1DXly2O8BIjCHeAu01vBA3OUsBSaH2OblxH2mt3cTTgki', TRUE),
+('Jessie', 'Wright', 'jessie.wright@gmail.com', '+16044013694','$2b$10$EY5gfFrs1DXly2O8BIjCHeAu01vBA3OUsBSaH2OblxH2mt3cTTgki', TRUE),
+('Sally', 'Fieldman', 'sally@example.com', '','$2b$10$EY5gfFrs1DXly2O8BIjCHeAu01vBA3OUsBSaH2OblxH2mt3cTTgki', FALSE),
+('Mike', 'Doorman', 'mike@example.com', '','$2b$10$EY5gfFrs1DXly2O8BIjCHeAu01vBA3OUsBSaH2OblxH2mt3cTTgki', FALSE),
+('Duck', 'Baron', 'baron@example.com', '+16044013694','$2b$10$EY5gfFrs1DXly2O8BIjCHeAu01vBA3OUsBSaH2OblxH2mt3cTTgki', TRUE)
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,42 @@
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcrypt');
+
 
 module.exports = (db) => {
+
+  router.post('/login', (req, res) => {
+    const email = req.body.email;
+    console.log(email);
+    db.query(`SELECT id, is_admin, password FROM users WHERE email = $1`, [email])
+      .then( data => {
+        if (!data.rows[0]) {
+          res.status(403).json({message: 'No account associated with this email'});
+        } else if(!bcrypt.compareSync(req.body.password, data.rows[0].password)) {
+
+          res.status(403).json({message: 'Incorrect Password'});
+        } else {
+          req.session.user_id = data.rows[0].id;
+          req.session.is_admin = data.rows[0].is_admin;
+          res.redirect('/');
+        }
+
+      })
+  });
+//todo check if email already registered first
+  router.post('/register', (req, res) => {
+    const queryParams = [req.body.firstName, req.body.lastName, req.body.email, req.body.phone, bcrypt.hashSync(req.body.password,10), req.body.is_admin];
+
+    db.query(`INSERT INTO users (first_name, last_name, email, phone, password, is_admin) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`, queryParams)
+      .then(data => {
+        req.session.user_id = data.rows[0].id;
+        req.session.is_admin = data.rows[0].is_admin;
+        res.redirect('/');
+
+      })
+
+  })
+
   router.get('/login/:id', (req, res) => {
     req.session.user_id = req.params.id;
     db.query('SELECT is_admin FROM users WHERE id = $1', [req.params.id]).then(function (data) {
@@ -18,3 +53,5 @@ module.exports = (db) => {
   });
   return router;
 };
+
+
